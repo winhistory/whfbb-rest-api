a225afe700ff5a1e52657b0aa6290a106fa7e4d1
Thomas Lercher <me@tlercher.de>
2016-05-26 13:52:37 +0200

    Add Controller & Repository for Threads

---
+++ src/main/java/net/winhistory/forum/controller/ThreadController.java
@@ -0,0 +1,34 @@
+package net.winhistory.forum.controller;
+
+import net.winhistory.forum.domain.Thread;
+import net.winhistory.forum.repository.ThreadRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/threads")
+public class ThreadController {
+
+    private final ThreadRepository threadRepository;
+
+    @Autowired
+    public ThreadController(ThreadRepository threadRepository) {
+        this.threadRepository = threadRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Page<Thread> listThreads(Pageable p) {
+        return threadRepository.findAll(p);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Thread getThread(@PathVariable final String id) {
+        return threadRepository.findOne(new ObjectId(id));
+    }
+}
--- src/main/java/net/winhistory/forum/repository/ThreadRepository.java
+++ src/main/java/net/winhistory/forum/repository/ThreadRepository.java
@@ -1,9 +1,8 @@
 package net.winhistory.forum.repository;
 
-
-import net.winhistory.forum.domain.Forum;
+import net.winhistory.forum.domain.Thread;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ForumRepository extends MongoRepository<Forum, ObjectId> {
+public interface ThreadRepository extends MongoRepository<Thread, ObjectId> {
 }
